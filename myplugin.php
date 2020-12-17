d850f19bb103ba3aea859bd367fe5e4bba0f8123
YEMING LIU <liuyeming@zhouyang.net>
2020-12-17 11:21:05 +0800

    init commit

---
+++ myplugin.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name:  MyPlugin
+Description:  Plugin Boilerplate
+Plugin URI:   https://jamesliu666.com
+Author:       James Liu
+Version:      1.0
+Text Domain:  myplugin
+Domain Path:  /languages
+License:      GPL v2 or later
+License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+
+// exit if file is called directly
+if ( ! defined( 'ABSPATH' ) ) {
+
+    exit;
+    
+}
+
